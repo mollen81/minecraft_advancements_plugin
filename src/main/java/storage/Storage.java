5d0d599c76ef7f45d9b7b35fe555be5339890457
mollen <stepanpanenko@gmail.com>
2026-01-20 21:04:44 +0300

    Storage.java

---
+++ src/main/java/storage/Storage.java
@@ -0,0 +1,13 @@
+package storage;
+
+import org.bukkit.NamespacedKey;
+
+import java.util.UUID;
+
+public class Storage
+{
+    public void addAdvancement(UUID uuid, NamespacedKey key, int attempt)
+    {
+
+    }
+}
