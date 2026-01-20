c53520c34d8b5c68c28ee78468575d9a67bd616a
mollen <stepanpanenko@gmail.com>
2026-01-20 21:03:43 +0300

    PlayerAdvancementListener.java

--- src/main/java/listeners/PlayerAdvancementListener.java
+++ src/main/java/listeners/PlayerAdvancementListener.java
@@ -1,27 +1,42 @@
 package listeners;
 
+import org.bukkit.NamespacedKey;
+import org.bukkit.advancement.Advancement;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerAdvancementDoneEvent;
-import plugin.AdvancementsTracker;
+import player.PlayerMode;
+import player.PlayerProfile;
+import player.PlayerProfileService;
+import progress.ProgressService;
 
 public class PlayerAdvancementListener implements Listener
 {
-    private final AdvancementsTracker plugin;
+    private final PlayerProfileService playerProfileService;
+    private final ProgressService progressService;
 
-    public PlayerAdvancementListener(AdvancementsTracker plugin)
+    public PlayerAdvancementListener(PlayerProfileService playerProfileService, ProgressService progressService)
     {
-        this.plugin = plugin;
+        this.playerProfileService = playerProfileService;
+        this.progressService = progressService;
     }
 
     //handler
-    @EventHandler
+    @EventHandler(ignoreCancelled = true)
     public void onPlayerAdvancement(PlayerAdvancementDoneEvent event)
     {
         Player player = event.getPlayer();
+        PlayerProfile profile = playerProfileService.get(player);
 
-        // actions after event
-        player.;
+        if(profile.getPlayerMode() == PlayerMode.DISABLED)
+        {
+            return;
+        }
+
+        Advancement advancement = event.getAdvancement();
+        NamespacedKey key = advancement.getKey();
+
+        progressService.completeAdvancement(player, key);
     }
 }
