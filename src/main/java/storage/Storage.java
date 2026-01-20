882f98900e1321e9f4ca09d22d2cb39780a97af6
mollen <stepanpanenko@gmail.com>
2026-01-20 22:13:00 +0300

    PlayerProfileService.java, Storage.java interface

--- src/main/java/storage/Storage.java
+++ src/main/java/storage/Storage.java
@@ -1,13 +1,20 @@
 package storage;
 
 import org.bukkit.NamespacedKey;
+import player.PlayerProfile;
 
+import java.util.Set;
 import java.util.UUID;
 
-public class Storage
+public interface Storage
 {
-    public void addAdvancement(UUID uuid, NamespacedKey key, int attempt)
-    {
+    PlayerProfile loadProfile(UUID uuid);
 
-    }
+    void saveProfile(PlayerProfile playerProfile);
+
+    Set<NamespacedKey> loadAdvancements(UUID uuid, int attempt);
+
+    public void addAdvancement(UUID uuid, NamespacedKey key, int attempt);
+
+    void clearAdvancements(UUID uuid, int attempt);
 }
