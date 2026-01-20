f342554eec995f94c60e07612d30d1fa84e31198
mollen <stepanpanenko@gmail.com>
2026-01-20 21:04:32 +0300

    Sidebar

--- src/main/java/sidebar/Sidebar.java
+++ src/main/java/sidebar/Sidebar.java
@@ -1,4 +1,46 @@
 package sidebar;
 
-public class Sidebar {
+import fr.mrmicky.fastboard.FastBoard;
+import org.bukkit.entity.Player;
+import player.PlayerMode;
+import player.PlayerProfile;
+import progress.ProgressService;
+import util.TimeFormatter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sidebar
+{
+    private final FastBoard board;
+    private final Player player;
+
+    public Sidebar(FastBoard board, Player player)
+    {
+        this.board = board;
+        this.player = player;
+    }
+
+
+    public void update(PlayerProfile profile, ProgressService progressService)
+    {
+        if(profile.getPlayerMode() == PlayerMode.DISABLED)
+        {
+            board.delete();
+            return;
+        }
+
+        List<String> lines = new ArrayList<>();
+        lines.add("[ Advancements Plugin ]");
+
+        if(profile.getPlayerMode() == PlayerMode.HARDCORE)
+        {
+            lines.add("Attempt: " + profile.getAttempt());
+            lines.add("Attempt time: " + TimeFormatter.format(profile.getAttemptTime()));
+        }
+
+        lines.add("Progress : " + progressService.)
+
+        board.updateLines();
+    }
 }
--- src/main/java/sidebar/SidebarManager.java
+++ src/main/java/sidebar/SidebarManager.java
@@ -1,4 +1,33 @@
 package sidebar;
 
-public class SidebarManager {
+import fr.mrmicky.fastboard.FastBoard;
+import org.bukkit.entity.Player;
+
+import java.util.Map;
+import java.util.UUID;
+
+public class SidebarManager
+{
+    private Map<UUID, Sidebar> sidebars;
+
+    public void create(Player player)
+    {
+        FastBoard newFastBoard = new FastBoard(player);
+        Sidebar newSidebar = new Sidebar(newFastBoard, player);
+
+        sidebars.put(player.getUniqueId(), newSidebar);
+    }
+
+    public void remove(Player player)
+    {
+        sidebars.remove(player.getUniqueId());
+    }
+
+    public void tick()
+    {
+        for(Sidebar sidebar : sidebars.values())
+        {
+            sidebar.update();
+        }
+    }
 }
