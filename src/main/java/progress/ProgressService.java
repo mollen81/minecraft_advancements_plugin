26b8be8b082b792197d2549958f7c51f14b39db1
mollen <stepanpanenko@gmail.com>
2026-01-21 17:29:15 +0300

    ProgressService.java

--- src/main/java/progress/ProgressService.java
+++ src/main/java/progress/ProgressService.java
@@ -1,21 +1,28 @@
 package progress;
 
-import animations.Spiral;
+import io.papermc.paper.advancement.AdvancementDisplay;
+import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import player.PlayerProfile;
 import player.PlayerProfileService;
 import storage.Storage;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ProgressService
 {
     private final PlayerProfileService profileService;
     private final Storage storage;
 
+    private final Set<NamespacedKey> allAdvancements = new HashSet<>();
+
     public ProgressService(PlayerProfileService profileService, Storage storage)
     {
         this.profileService = profileService;
         this.storage = storage;
+        cacheAdvancements();
     }
 
     public void completeAdvancement(Player player, NamespacedKey key)
@@ -38,13 +45,33 @@ public class ProgressService
     }
 
 
-    public Double getProgressPercent(Player player)
-    {
-        return null;
-    }
-
     public Integer getCompletedCount(PlayerProfile profile)
     {
         return profile.getCompletedAdvancements().size();
+    }
+
+
+    private void cacheAdvancements()
+    {
+        Bukkit.advancementIterator().forEachRemaining(adv -> {
+
+            NamespacedKey key = adv.getKey();
+
+            // только ваниль
+            if (!key.getNamespace().equals("minecraft")) return;
+
+            // исключаем рецепты
+            if (key.getKey().startsWith("recipes/")) return;
+
+            // исключаем root (story/root, nether/root, end/root)
+            if (key.getKey().endsWith("/root")) return;
+
+            /* опционально: скрытые
+            AdvancementDisplay display = adv.getDisplay();
+            if (display != null && display.isHidden()) return;
+            */
+
+            allAdvancements.add(key);
+        });
     }
 }
