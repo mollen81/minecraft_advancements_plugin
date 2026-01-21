19d697c09400747c843d18d6bf5b14b348af1887
mollen <stepanpanenko@gmail.com>
2026-01-21 16:52:33 +0300

    TimeFormatter.java updated

--- src/main/java/util/TimeFormatter.java
+++ src/main/java/util/TimeFormatter.java
@@ -4,15 +4,10 @@ public class TimeFormatter
 {
     public static String format(long seconds)
     {
-        if (seconds < 60)
-        {
-            return (Long.toString(seconds));
-        }
-        else if(seconds < 3600)
-        {
-            return (Long.toString(seconds/60) + ":" + Long.toString(seconds));
-        }
+        long hours = seconds / 3600;
+        long minutes = seconds / 60;
+        long sec = seconds % 60;
 
-        return (Long.toString(seconds/3600) + ":" + Long.toString(seconds/60) + ":" + Long.toString(seconds));
+        return String.format("%d:%d:%d", hours, minutes, sec);
     }
 }
